cf811cdd7fcba4702142afdd561e1e431329a8c0
s-doris <swdoriz@gmail.com>
2020-12-23 23:33:09 +0900

    feat : Base intro view init

---
+++ ironman/Intro/ServiceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ServiceViewController.swift
+//  ironman
+//
+//  Created by once on 2020/12/23.
+//
+
+import UIKit
+
+class ServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
