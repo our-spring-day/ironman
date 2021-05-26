1b338cc5aec2ef22f62cd1d05e74277ce1e59dd7
jeongjaein <wodls96@gmail.com>
2021-05-26 16:18:33 +0900

    feat(...) : XmarkButton 클래스 추가

---
+++ ironman/Common/XmarkButton.swift
@@ -0,0 +1,23 @@
+//
+//  XmarkButton.swift
+//  ironman
+//
+//  Created by 정재인 on 2021/05/26.
+//
+
+import UIKit
+
+class XmarkButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        self.do {
+            $0.setImage(UIImage(systemName: "xmark"), for: .normal)
+            $0.tintColor = .black
+            $0.alpha = 0.2
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
