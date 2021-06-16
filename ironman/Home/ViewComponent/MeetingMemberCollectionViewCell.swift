633ddfaa8d8c075af832651db51e3972cce470f0
jeongjaein <wodls96@gmail.com>
2021-06-16 21:13:57 +0900

    feat(...) : MeetingMemberCollectionViewCell 클래스 추가

--- ironman/Home/ViewComponent/MeetingMemberCollectionViewCell.swift
+++ ironman/Home/ViewComponent/MeetingMemberCollectionViewCell.swift
@@ -5,4 +5,37 @@
 //  Created by 정재인 on 2021/06/16.
 //
 
-import Foundation
+import UIKit
+
+class MeetingMemberCollectionViewCell: UICollectionViewCell {
+    let profileImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: attribute & layout
+
+extension MeetingMemberCollectionViewCell {
+    func attribute() {
+        profileImageView.do {
+            $0.setRadiusWithMasksToBounds(19)
+            $0.contentMode = .scaleAspectFill
+        }
+    }
+    
+    func layout() {
+        [profileImageView].forEach { addSubview($0)}
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
